5298d338f0913ccc6e46134dad8f8067351c71fb
Demir <demirberkayyilmaz@gmail.com>
2016-01-16 23:11:00 -0800

    tab bar icons

--- iOS App/WarMachine/WarMachine/ViewController.swift
+++ iOS App/WarMachine/WarMachine/ViewController.swift
@@ -61,9 +61,9 @@ class ViewController: UIViewController {
         
         var descript = "T.Masthay kicks 74 yards from GB 35 to MIN -9. C.Patterson for 109 yards, TOUCHDOWN."
         
-        createNewEntry("Awesome Playerr", imageOneName: "testimg1.png", imageTwoName:  "testimg2.png", homeScore: "6", awayScore: "0", description: descript, yardLine: "25")
-        createNewEntry("This Shit Sucks", imageOneName: "testimg1.png", imageTwoName:  "testimg2.png", homeScore: "8", awayScore: "0", description: descript, yardLine: "45")
-        createNewEntry("Go Pantherss", imageOneName: "testimg1.png", imageTwoName:  "testimg2.png", homeScore: "12", awayScore: "0", description: descript, yardLine: "65")
+        createNewEntry("Awesome Playerr yeaa madafka yooogr regaerg", imageOneName: "testimg1.png", imageTwoName:  "testimg2.png", homeScore: "6", awayScore: "0", description: descript, yardLine: "25")
+        createNewEntry("This Shit Sucks I hate this game ergrer", imageOneName: "testimg1.png", imageTwoName:  "testimg2.png", homeScore: "8", awayScore: "0", description: descript, yardLine: "45")
+        createNewEntry("Go Pantherss gooo wuuuhuu wuuu ooo ii uu yy ", imageOneName: "testimg1.png", imageTwoName:  "testimg2.png", homeScore: "12", awayScore: "0", description: descript, yardLine: "65")
     }
     
     func createNewEntry(title: String, imageOneName: String, imageTwoName: String, homeScore: String, awayScore: String, description: String, yardLine: String) {
@@ -72,58 +72,74 @@ class ViewController: UIViewController {
 //        element.backgroundColor = UIColor.whiteColor()
         element.addBorder(width: 1, color: UIColor.grayColor())
         
-        let backgroundImage = UIImage(named: "intro-1.png")
-        let backgroundImageView = UIImageView(image: backgroundImage!)
-        backgroundImageView.frame = CGRect(x: 0, y: 0, width: ez.screenWidth, height: ez.screenHeight)
-        backgroundImageView.alpha = 0.5
-        element.addSubview(backgroundImageView)
+//        let backgroundImage = UIImage(named: "intro-1.png")
+//        let backgroundImageView = UIImageView(image: backgroundImage!)
+//        backgroundImageView.frame = CGRect(x: 0, y: 0, width: ez.screenWidth, height: ez.screenHeight)
+//        backgroundImageView.alpha = 0.5
+//        element.addSubview(backgroundImageView)
         
-        let imageOne = UIImage(named: imageOneName)
-        let imageViewOne = UIImageView(image: imageOne!)
-        imageViewOne.frame = CGRect(x: 20, y: 50, width: 100, height: 100)
-        element.addSubview(imageViewOne)
+        let myGif = UIImage.gifWithName("mugen")
+        let gifView = UIImageView(x: 0, y: 150, w: ez.screenWidth, h: 400, image: myGif!)
+        element.addSubview(gifView)
+        element.sendSubviewToBack(gifView)
+        
+        let topImage = UIImage(named: "top.png")
+        let topImageView = UIImageView(image: topImage!)
+        topImageView.frame = CGRect(x: 0, y: 0, width: ez.screenWidth, height: 165)
+        element.addSubview(topImageView)
+        
+        let bottomImage = UIImage(named: "bottom.png")
+        let bottomImageView = UIImageView(image: bottomImage!)
+        bottomImageView.frame = CGRect(x: 0, y: ez.screenHeight - 220, width: ez.screenWidth, height: 220)
+        element.addSubview(bottomImageView)
+        
+        let playerImage = UIImage(named: imageOneName)
+        let playerImageView = UIImageView(image: playerImage!)
+        playerImageView.frame = CGRect(x: 10, y: 20, width: 100, height: 100)
+        playerImageView.round()
+        element.addSubview(playerImageView)
         
 //        let imageTwo = UIImage(named: imageTwoName)
 //        let imageViewTwo = UIImageView(image: imageTwo!)
 //        imageViewTwo.frame = CGRect(x: ez.screenWidth - 20 - 100, y: 200, width: 100, height: 100)
 //        element.addSubview(imageViewTwo)
         
-        let label = UILabel(frame: CGRect(x: 0, y: 30, width: 0, height: 100))
+        let label = UILabel(frame: CGRect(x: 0, y: 5, width: 0, height: 100))
         label.textColor = UIColor.blackColor()
-        label.font = UIFont(name: label.font.fontName, size: 30)
+        label.font = UIFont(name: label.font.fontName, size: 24)
+        label.numberOfLines = 0
         label.text = title
         label.resizeToFitWidth()
-        label.x = imageViewOne.x + imageViewOne.w + 30
+        label.x = playerImageView.x + playerImageView.w
+        label.w = ez.screenWidth - label.x - 20
         element.addSubview(label)
         
-        let homeScoreLabel = UILabel(frame: CGRect(x: 0, y: 140, width: 0, height: 100))
-        homeScoreLabel.textColor = UIColor.blackColor()
-        homeScoreLabel.font = UIFont(name: homeScoreLabel.font.fontName, size: 25)
-        homeScoreLabel.text = "Home Score: " + homeScore + " / Away Score: " + awayScore
-        homeScoreLabel.resizeToFitWidth()
-        homeScoreLabel.x = (ez.screenWidth - homeScoreLabel.w)/2
-        element.addSubview(homeScoreLabel)
+//        let homeScoreLabel = UILabel(frame: CGRect(x: 0, y: 140, width: 0, height: 100))
+//        homeScoreLabel.textColor = UIColor.blackColor()
+//        homeScoreLabel.font = UIFont(name: homeScoreLabel.font.fontName, size: 25)
+//        homeScoreLabel.text = "Home Score: " + homeScore + " / Away Score: " + awayScore
+//        homeScoreLabel.resizeToFitWidth()
+//        homeScoreLabel.x = (ez.screenWidth - homeScoreLabel.w)/2
+//        element.addSubview(homeScoreLabel)
+//        
+//        let yardLineLabel = UILabel(frame: CGRect(x: 0, y: 170, width: 0, height: 100))
+//        yardLineLabel.textColor = UIColor.blackColor()
+//        yardLineLabel.font = UIFont(name: yardLineLabel.font.fontName, size: 25)
+//        yardLineLabel.text = "Yardline: " + yardLine
+//        yardLineLabel.resizeToFitWidth()
+//        yardLineLabel.x = (ez.screenWidth - yardLineLabel.w)/2
+//        element.addSubview(yardLineLabel)
+//        
+//        let descriptionLabel = UILabel(frame: CGRect(x: 0, y: 350, width: ez.screenWidth - 40, height: 0))
+//        descriptionLabel.textColor = UIColor.blackColor()
+//        descriptionLabel.font = UIFont(name: descriptionLabel.font.fontName, size: 25)
+//        descriptionLabel.numberOfLines = 0
+//        descriptionLabel.text = description
+//        descriptionLabel.resizeToFitHeight()
+//        descriptionLabel.x = 20
+//        element.addSubview(descriptionLabel)
         
-        let yardLineLabel = UILabel(frame: CGRect(x: 0, y: 170, width: 0, height: 100))
-        yardLineLabel.textColor = UIColor.blackColor()
-        yardLineLabel.font = UIFont(name: yardLineLabel.font.fontName, size: 25)
-        yardLineLabel.text = "Yardline: " + yardLine
-        yardLineLabel.resizeToFitWidth()
-        yardLineLabel.x = (ez.screenWidth - yardLineLabel.w)/2
-        element.addSubview(yardLineLabel)
         
-        let descriptionLabel = UILabel(frame: CGRect(x: 0, y: 350, width: ez.screenWidth - 40, height: 0))
-        descriptionLabel.textColor = UIColor.blackColor()
-        descriptionLabel.font = UIFont(name: descriptionLabel.font.fontName, size: 25)
-        descriptionLabel.numberOfLines = 0
-        descriptionLabel.text = description
-        descriptionLabel.resizeToFitHeight()
-        descriptionLabel.x = 20
-        element.addSubview(descriptionLabel)
-        
-        let myGif = UIImage.gifWithName("mugen")
-        let gifView = UIImageView(x: 0, y: 300, w: ez.screenWidth, h: 300, image: myGif!)
-        element.addSubview(gifView)
         
         nowY = nowY + element.h
         scrollView.addSubview(element)
