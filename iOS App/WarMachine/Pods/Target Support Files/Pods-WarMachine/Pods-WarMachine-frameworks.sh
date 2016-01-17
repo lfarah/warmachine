f2a1f1557e1d0208ae35e2529650368894039605
Demir <demirberkayyilmaz@gmail.com>
2016-01-16 22:13:17 -0800

    select screen & main screen done

--- iOS App/WarMachine/Pods/Target Support Files/Pods-WarMachine/Pods-WarMachine-frameworks.sh
+++ iOS App/WarMachine/Pods/Target Support Files/Pods-WarMachine/Pods-WarMachine-frameworks.sh
@@ -85,9 +85,11 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods-WarMachine/EZSwiftExtensions.framework"
+  install_framework "Pods-WarMachine/Gifu.framework"
   install_framework "Pods-WarMachine/SAParallaxViewControllerSwift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods-WarMachine/EZSwiftExtensions.framework"
+  install_framework "Pods-WarMachine/Gifu.framework"
   install_framework "Pods-WarMachine/SAParallaxViewControllerSwift.framework"
 fi
