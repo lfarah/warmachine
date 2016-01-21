083fb5b1c06a86c0f15492c315448c345f6b8217
Lucas Farah <lucas.farah@me.com>
2016-01-20 17:41:34 -0800

    Added navigation, onboarding

--- iOS App/Nubee/Nubee/AppDelegate.swift
+++ iOS App/Nubee/Nubee/AppDelegate.swift
@@ -16,6 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
     // Override point for customization after application launch.
+    
+    //Setting Navigation Bar Background Image
+    let backgroundImage = UIImage(named: "Top Bar")
+    UINavigationBar.appearance().setBackgroundImage(backgroundImage, forBarMetrics: .Default)
+
     return true
   }
 
