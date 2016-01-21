fbf6327184ad6fe6eed92868d67baf51a125495e
Lucas Farah <lucas.farah@me.com>
2016-01-20 17:06:47 -0800

    Removed iOS Project

--- iOS App/WarMachine/Pods/Target Support Files/Pods-WarMachine/Pods-WarMachine-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WarMachine : NSObject
-@end
-@implementation PodsDummy_Pods_WarMachine
-@end
--- iOS App/WarMachine/WarMachine/TabBar.swift
+++
@@ -1,9 +0,0 @@
-//
-//  TabBar.swift
-//  WarMachine
-//
-//  Created by Troy Do on 1/16/16.
-//  Copyright © 2016 Lucas Farah. All rights reserved.
-//
-
-import Foundation
