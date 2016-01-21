20d59ded3e22f6190d4dd9e0d85d8781514138a4
Lucas Farah <lucas.farah@me.com>
2016-01-20 16:51:20 -0800

    Project created

--- iOS App/WarMachine/Pods/Target Support Files/SAParallaxViewControllerSwift/SAParallaxViewControllerSwift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SAParallaxViewControllerSwift : NSObject
-@end
-@implementation PodsDummy_SAParallaxViewControllerSwift
-@end
--- iOS App/WarMachine/Pods/Target Support Files/SAParallaxViewControllerSwift/SAParallaxViewControllerSwift-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double SAParallaxViewControllerSwiftVersionNumber;
-FOUNDATION_EXPORT const unsigned char SAParallaxViewControllerSwiftVersionString[];
-
