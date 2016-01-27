abb84f80407476e70ccb795158034764a4210e5b
troydo42 <hoangdov@gmail.com>
2016-01-27 15:51:19 -0800

    successfully pull game data from NFL

--- NFLScraper/nfl.py
+++ NFLScraper/nfl.py
@@ -1,12 +1,24 @@
+"""Get Play PLay data"""
+#
 import nflgame
+import json
 
-games = nflgame.games(2015, week = 10 )
+games = nflgame.games(2015, week = 1)
 plays = nflgame.combine_plays(games)
-for p in plays.sort('passing_yds').limit(5):
+for p in plays.limit(10000):
     print p
+
+
+
+
+
+# import nfldb
+# import nflvid.vlc
+#
 # db = nfldb.connect()
 # q = nfldb.Query(db)
 #
 # q.game(season_year=2012, season_type='Regular')
-# for pp in q.sort('passing_yds').limit(10).as_aggregate():
-#     print pp.player, pp.passing_yds
+# q.player(full_name='Adrian Peterson').play(rushing_yds__ge=50)
+#
+# nflvid.vlc.watch(db, q.as_plays(), '/m/nfl/coach/pbp')
