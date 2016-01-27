e8e6bc5bd0822145144d0d6d02606dcd5cf4f545
troydo42 <hoangdov@gmail.com>
2016-01-27 15:13:19 -0800

    setup NFL Scraper

---
+++ NFLScraper/nfl.py
@@ -0,0 +1,10 @@
+import nflgame
+
+games = nflgame.sched(2015, week = 10 )
+print games.d
+# db = nfldb.connect()
+# q = nfldb.Query(db)
+#
+# q.game(season_year=2012, season_type='Regular')
+# for pp in q.sort('passing_yds').limit(10).as_aggregate():
+#     print pp.player, pp.passing_yds
